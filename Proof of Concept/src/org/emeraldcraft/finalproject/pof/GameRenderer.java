69d18801ed5d8329253655c740d656adb6fbfcc7
EmeraldWither <bikechamp007@gmail.com>
2023-05-16 20:33:16 -0400

    I MADE A MAIN MENU

--- Proof of Concept/src/org/emeraldcraft/finalproject/pof/GameRenderer.java
+++ Proof of Concept/src/org/emeraldcraft/finalproject/pof/GameRenderer.java
@@ -5,7 +5,13 @@ import org.emeraldcraft.finalproject.pof.utils.Logger;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 
 import static org.emeraldcraft.finalproject.pof.DebugValues.SHOW_HITBOXES;
 
@@ -13,19 +19,69 @@ public class GameRenderer extends JComponent {
     private final SegalGame game = SegalGame.getInstance();
     private boolean isRunning = false;
     private final JPanel panel;
-    public GameRenderer(JPanel panel) {
+    private final JFrame frame;
+    private final JFrame gameFrame = new JFrame();
+    public GameRenderer(JPanel panel, JFrame frame) {
     	this.panel = panel;
+        this.frame = frame;
     }
     @Override
     public void paintComponent(Graphics g) {
-    	if(SegalGame.getInstance().isMainMenu()) return;
+    	if(SegalGame.getInstance().isMainMenu()){
+            return;
+        }
         paintGame(g);
     }
+    public void switchToGame(){
+        Logger.log("Main game sequence started. Destroying the old JFrame and creating the new one");
+        frame.dispose();
+        gameFrame.add(this);
+        gameFrame.setName("Seagull Swipe");
+        gameFrame.setSize(1920, 1040);
+        gameFrame.setUndecorated(true);
+        gameFrame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        gameFrame.addKeyListener(new KeyListener() {
+            //Special type of list which can only hold unique values
+            private final Set<Character> keys = new HashSet<>();
+
+            @Override
+            public void keyTyped(KeyEvent e) {
+
+            }
+
+            @Override
+            public void keyPressed(KeyEvent e) {
+                keys.add(e.getKeyChar());
+                handleKeys();
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+                keys.remove(e.getKeyChar());
+            }
+
+            public void handleKeys() {
+                for (char key : keys) {
+                    //Logger.log("Key Event Fired! Char = " + e.getKeyChar());
+                    int x = 0, y = 0;
+                    if (key == 'a') x -= 10;
+                    else if (key == 'd') x += 10;
+                    else if (key == 'w') y -= 10;
+                    else if (key == 's') y += 10;
+                    game.getPlayer().control(x, y);
+                }
+            }
+        });
+        gameFrame.setVisible(true);
+
+        SegalGame.getInstance().start();
+
+    }
     public void paintMenu() {
-    	GridLayout layout = new GridLayout(5, 3);
-    	panel.setLayout(layout);
+        GridLayout layout = new GridLayout(5, 3);
+        panel.setLayout(layout);
         panel.setAlignmentX(Component.CENTER_ALIGNMENT);
-    	
+
         Panel textPanel = new Panel();
 
         JLabel label = new JLabel("Sandwich Swipe");
@@ -36,21 +92,45 @@ public class GameRenderer extends JComponent {
         textPanel.add(label, new GridBagConstraints());
         panel.add(textPanel);
         //Play Now Button
-    	JButton playNow = new JButton("Play");
-    	playNow.setFont(new Font("Arial", Font.BOLD, 32));
-    	playNow.setPreferredSize(new Dimension(100, 100));
-    	playNow.setAlignmentX(JButton.CENTER_ALIGNMENT);
+        JButton playNow = new JButton("Play");
+        playNow.setFont(new Font("Arial", Font.BOLD, 32));
+        playNow.setPreferredSize(new Dimension(100, 100));
+        playNow.setAlignmentX(JButton.CENTER_ALIGNMENT);
+        playNow.addMouseListener(new MouseListener() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                switchToGame();
+            }
 
-    	panel.add(playNow, new GridBagConstraints() );
-    	
-    	
-    	//Settings Button
-    	JButton settings = new JButton("Settings");
-    	settings.setFont(new Font("Arial", Font.BOLD, 32));
-    	settings.setPreferredSize(new Dimension(100, 100));
-    	settings.setAlignmentX(JButton.CENTER_ALIGNMENT);
+            @Override
+            public void mousePressed(MouseEvent e) {
 
-    	panel.add(settings, new GridBagConstraints());
+            }
+            @Override
+            public void mouseReleased(MouseEvent e) {
+
+            }
+            @Override
+            public void mouseEntered(MouseEvent e) {
+
+            }
+            @Override
+            public void mouseExited(MouseEvent e) {
+
+            }
+        });
+
+        panel.add(playNow, new GridBagConstraints() );
+
+
+        //Settings Button
+        JButton settings = new JButton("Settings");
+        settings.setFont(new Font("Arial", Font.BOLD, 32));
+        settings.setPreferredSize(new Dimension(100, 100));
+        settings.setAlignmentX(JButton.CENTER_ALIGNMENT);
+
+        panel.add(settings, new GridBagConstraints());
+        frame.add(panel);
     }
     public void paintGame(Graphics g) {
     	ArrayList<GameObject> gameObjects = game.getGameObjects();
@@ -70,8 +150,7 @@ public class GameRenderer extends JComponent {
         }
     }
 
-    public void start()
-    {
+    public void start() {
         if(isRunning) throw new IllegalStateException("The renderer has already been started and is running.");
         isRunning = true;
         Logger.log("Game Renderer has been initialized and is running.");
