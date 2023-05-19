4f0476cd7dcc88e6247775b22fd45241cd4dd0e1
EmeraldWither <bikechamp007@gmail.com>
2023-05-18 21:34:31 -0400

    diving logic

--- Proof of Concept/src/org/emeraldcraft/finalproject/pof/gameobjects/Player.java
+++ Proof of Concept/src/org/emeraldcraft/finalproject/pof/gameobjects/Player.java
@@ -1,27 +1,25 @@
 package org.emeraldcraft.finalproject.pof.gameobjects;
 
-import java.awt.Graphics;
-import java.awt.Image;
-import java.awt.Rectangle;
-import java.io.File;
-import java.io.IOException;
-
-import javax.imageio.ImageIO;
-
 import org.emeraldcraft.finalproject.pof.SegalGame;
 import org.emeraldcraft.finalproject.pof.components.Controllable;
 import org.emeraldcraft.finalproject.pof.components.GameObject;
 import org.emeraldcraft.finalproject.pof.utils.Logger;
 
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+
 public class Player extends GameObject implements Controllable {	
-	private boolean flying = true;
+	private final boolean flying = true;
 	private boolean currentlyJumping = false;
-	private boolean currentlyDiving = false;
-	private boolean divingUp = false;
 	private int jumpCounter = 0;
 	private boolean jumpingArch = false;
 	
 	private final Image image;
+	private boolean isDiving = false;
+	private boolean divingDown = true;
+
 	public Player() throws IOException {
 		super("The Player", new Rectangle(240, 90), 1);
 		File file = new File("seagull.png");
@@ -49,9 +47,9 @@ public class Player extends GameObject implements Controllable {
 		//The code above will prevent the seagull from going off the screen
 		
 	}
-	public void dive(boolean isDivingUp) {
-		currentlyDiving = true;
-		this.divingUp = divingUp;
+	public void dive() {
+		isDiving = true;
+		divingDown = true;
 	}
 	
 	public void jump(boolean isJumping) {
@@ -61,32 +59,48 @@ public class Player extends GameObject implements Controllable {
 	@Override
 	public void tick() {
 		eatingLogic();
-		
-		//Diving method logic
-		
-		if(getLocation().y < 1900 && currentlyDiving && !divingUp) {
-			getLocation().y++;
-			getLocation().x++;
+		walkLogic();
+		divingLogic();
+	}
+	private void divingLogic(){
+		//dont do anything if we are not diving
+		if(!isDiving) return;
+
+		//bounds check
+		if(getLocation().x + getLocation().width >= 1920){
+			isDiving = false;
+			return;
 		}
-		if(currentlyDiving && divingUp) {
-			getLocation().y--;
-			getLocation().x++;
+
+
+		if(divingDown && getLocation().y < 900){
+			getLocation().y += 25;
+			getLocation().x += 10;
 		}
-		//Jumping logic
+		//if we are at the bottom, then flip our diving direction
+		else if(divingDown && getLocation().y >= 900) divingDown = !divingDown;
+		else if(!divingDown && getLocation().y >= 100){
+			getLocation().y -= 15;
+			getLocation().x += 7;
+		}
+		else if (!divingDown && getLocation().y <= 300) {
+			isDiving = false;
+		}
+	}
+	private void walkLogic(){
 		if(getLocation().y >= 700 && currentlyJumping && getLocation().x <= 1650 && getLocation().x >= 0) {
 			//shLogger.log("The Jump command has been triggered");
-			if(jumpCounter < 140 && jumpingArch == false) {
+			if(jumpCounter < 140 && !jumpingArch) {
 				//Logger.log("jumping");
 				getLocation().y-=10;
 				getLocation().x+=4;
 				jumpCounter+=5;
-				Logger.log(jumpCounter + "");
 				if(jumpCounter >= 135) {
 					//Logger.log("if statement go burrr");
 					jumpingArch = true;
 				}
 			}
-			else if(jumpingArch == true) {
+			else if(jumpingArch) {
 //				Logger.log("falling");
 				getLocation().y+=10;
 				getLocation().x+=4;
@@ -99,8 +113,6 @@ public class Player extends GameObject implements Controllable {
 				}
 			}
 		}
-		
-		//Walking Logic
 	}
 	private void eatingLogic() {
 		for(GameObject object : SegalGame.getInstance().getGameObjects()) {
