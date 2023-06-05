3aca208aac62df3d6394c3b029b4f8f7df941b4a
GavinMcClure <gtmcclure9@gmail.com>
2023-06-05 13:11:19 -0400

    aaaa

--- Proof of Concept/src/org/emeraldcraft/finalproject/pof/GameSettings.java
+++ Proof of Concept/src/org/emeraldcraft/finalproject/pof/GameSettings.java
@@ -3,12 +3,12 @@ package org.emeraldcraft.finalproject.pof;
 public class GameSettings {
 	public static class StaminaSettings{
 		public static int FLY_PUNISHMENT = 1;
-		public static int JUMPING_PUNISHMENT = 4;
-		public static int WALKING_REWARD = 2;
-		public static int DIVING_PUNISHMENT = 4;
+		public static int JUMPING_PUNISHMENT = 5;
+		public static int WALKING_REWARD = 1;
+		public static int DIVING_PUNISHMENT = 3;
 	}
 	public static class CoreSettings {
-		public static int TICK_TIME = 15;
+		public static int TICK_TIME = 10;
 	}
 	public static class HumanSettings {
 		public static int HUMAN_MIN_SPAWN_TIME = 2;
