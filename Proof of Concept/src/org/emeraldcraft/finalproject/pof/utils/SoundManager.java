842c107920791852169cbfe5692dd48fd0ae9724
EmeraldWither <bikechamp007@gmail.com>
2023-06-05 16:17:38 -0400

    sound effects

---
+++ Proof of Concept/src/org/emeraldcraft/finalproject/pof/utils/SoundManager.java
@@ -0,0 +1,22 @@
+package org.emeraldcraft.finalproject.pof.utils;
+
+import java.io.File;
+
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+
+public class SoundManager {
+	public static Clip getSoundEffect(String name) {
+		try {
+			String location = "assets/sounds/" + name + ".wav";
+			Clip clip = AudioSystem.getClip();
+			clip.open(AudioSystem.getAudioInputStream(new File(location)));
+			return clip;
+		} catch (Exception e) {
+			Logger.warn("An error was thrown when trying to create sound effect for " + name + "!");
+			e.printStackTrace();
+			System.exit(-1);
+			return null;
+		}
+	}
+}
