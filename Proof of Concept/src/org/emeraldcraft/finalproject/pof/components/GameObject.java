55b2ef3ee761ec74969f7c48dcd64b447701c125
EmeraldWither <bikechamp007@gmail.com>
2023-06-06 17:21:12 -0400

    format all code

--- Proof of Concept/src/org/emeraldcraft/finalproject/pof/components/GameObject.java
+++ Proof of Concept/src/org/emeraldcraft/finalproject/pof/components/GameObject.java
@@ -1,3 +1,9 @@
+/*
+ * Ishaan Sayal && Gavin McClure
+ * Period 2
+ * 6/7/2023
+ */
+
 package org.emeraldcraft.finalproject.pof.components;
 
 import org.emeraldcraft.finalproject.pof.SegalGame;
@@ -5,42 +11,56 @@ import org.emeraldcraft.finalproject.pof.utils.Logger;
 
 import java.awt.*;
 
-public abstract class GameObject {
-	private final String name;
-	private final Rectangle location;
-	private final int renderPriority;
+public abstract class GameObject
+{
+    private final String name;
+    private final Rectangle location;
+    private final int renderPriority;
 
-	public GameObject(String name, Rectangle hitbox, int renderPriority) {
-		this.name = name;
-		this.location = hitbox;
-		this.renderPriority = renderPriority;
-		SegalGame.getInstance().registerGameObject(this);
-		Logger.log("Created a new GameObject of: " + this);
-	}
-	public Rectangle getLocation() {
-		return this.location;
-	}
-	public abstract boolean shouldRemove();
-	public void remove() {
-		Logger.log(this + " has been deregistered and has been removed.");
-		SegalGame.getInstance().deRegisterGameObject(this);
-	}
-	public String getName() {
-		return this.name;
-	}
-	@Override
-	public String toString() {
-		return "GameObject { name: \"" + name + "\", Location: " + getLocation().toString() + "}";
-	}
-	
-	public abstract void render(Graphics g);
-	public abstract void tick();
+    public GameObject(String name, Rectangle hitbox, int renderPriority)
+    {
+        this.name = name;
+        this.location = hitbox;
+        this.renderPriority = renderPriority;
+        SegalGame.getInstance().registerGameObject(this);
+        Logger.log("Created a new GameObject of: " + this);
+    }
 
-	public boolean canCollide(){
-		return false;
-	}
+    public Rectangle getLocation()
+    {
+        return this.location;
+    }
 
-	public int getRenderPriority() {
-		return renderPriority;
-	}
+    public abstract boolean shouldRemove();
+
+    public void remove()
+    {
+        Logger.log(this + " has been deregistered and has been removed.");
+        SegalGame.getInstance().deRegisterGameObject(this);
+    }
+
+    public String getName()
+    {
+        return this.name;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "GameObject { name: \"" + name + "\", Location: " + getLocation().toString() + "}";
+    }
+
+    public abstract void render(Graphics g);
+
+    public abstract void tick();
+
+    public boolean canCollide()
+    {
+        return false;
+    }
+
+    public int getRenderPriority()
+    {
+        return renderPriority;
+    }
 }
